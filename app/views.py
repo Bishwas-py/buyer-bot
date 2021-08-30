cf00a05f575e1ac776429fec5544bf3b8263edba
bishwas-py <bishwasbh@gmail.com>
2021-08-30 20:01:22 +0545

    Login required

--- app/views.py
+++ app/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from.models import Items
 from.machine.main import quit_all_bot, threader, threads, quit_bot, run_isolated_bot
 
+
+@login_required(login_url='/admin/login/')
 def home(request):
     return render(request, "bots/index.html", {"threads":threads})
 
+@login_required(login_url='/admin/login/')
 def start_bots(request):
     if not threads:
         threader()
     return redirect(reverse('dashboard'))
 
+@login_required(login_url='/admin/login/')
 def stop_bots(request, id:int):
     quit_bot(id)
     return redirect(reverse('dashboard'))
 
+@login_required(login_url='/admin/login/')
 def stop_all_bots(request):
     quit_all_bot()
     return redirect(reverse('dashboard'))
